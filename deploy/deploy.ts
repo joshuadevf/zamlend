ee523fbac4c6d8ae53423356c77a0f59d7fc8386
joshuadevf <ilfvi0257@outlook.com>
2025-12-24 18:26:19 +0800

    deploy: add ZamaLend + cZama deployment flow

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -3,15 +3,24 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
-  const { deploy } = hre.deployments;
+  const { deploy, execute } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedToken = await deploy("ConfidentialZama", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const deployedLend = await deploy("ZamaLend", {
+    from: deployer,
+    log: true,
+    args: [deployedToken.address],
+  });
+
+  await execute("ConfidentialZama", { from: deployer, log: true }, "setMinter", deployedLend.address);
+
+  console.log(`ConfidentialZama contract: `, deployedToken.address);
+  console.log(`ZamaLend contract: `, deployedLend.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_zama_lend"; // id required to prevent reexecution
+func.tags = ["ZamaLend"];
